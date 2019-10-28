9e1578aadf88433c074f2bcba3f06890f2dab2de
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-28 12:23:28 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.jh.vmv2;
+
+public class Main {
+	public static void main(String[] args) {
+		VendingMachine vm =new VendingMachine();
+		
+		vm.showDrinks();
+		vm.purchase(1, 10000);
+		vm.putProduct(1, 10);
+
+	}
+
+
+
+}
