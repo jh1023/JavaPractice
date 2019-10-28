3d1c980d12b5510eae14a41f3d3792345bb61c53
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-28 13:46:23 +0900

    Update Main.java

--- vending2/Main.java
+++ vending2/Main.java
@@ -3,9 +3,9 @@ package com.jh.vmv2;
 public class Main {
 	public static void main(String[] args) {
 		VendingMachine vm =new VendingMachine();
-		
-		vm.purchase(1, 10000);
+
 		vm.putProduct(1, 10);
+		vm.purchase(1, 10000);
 		vm.showDrinks();
 
 
