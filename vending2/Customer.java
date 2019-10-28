9736c8c93973093f5edbf6c0244279ad8f17ffe7
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-28 14:11:41 +0900

    Add files via upload

---
+++ vending2/Customer.java
@@ -0,0 +1,19 @@
+package com.jh.vmv2;
+
+import java.util.*;
+
+public class Customer {
+	
+	private List<Drink> drinks = new ArrayList();
+	private String name;
+	private int money;
+	
+	//생성자 - drinks는 벤딩머신과 같게 처리
+	//외부에서 들어오는 name, money값을 this name money 변수에 값을 넣어주는 생성자를 만드세요
+	
+	public Customer() {
+		
+	}
+	
+	
+}
