fda67cda143531e85a765fd7a4dec647b8760454
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-28 12:24:13 +0900

    Add files via upload

---
+++ vending2/Drink.java
@@ -0,0 +1,48 @@
+package com.jh.vmv2;
+
+public class Drink {
+
+	private String pName; // 제품명
+	private int price; // 가격
+	private int stock; // 재고
+
+	public Drink(String pName, int price) { // 생성자는 리턴타입x 클래스명이랑 같음
+
+		this.pName = pName;
+		this.price = price;
+	}
+
+	public String getpName() { // 내가 외부로 줘야하는 값.
+
+		return pName;
+	}
+
+	public void setpName(String pName) { // 외부에서 들어오는 값 세팅
+		this.pName = pName;
+	}
+
+	public int getPrice() {
+
+		return price;
+	}
+
+	public void setPrice(int price) {
+
+		this.price = price;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	public int getStock() {
+		return stock;
+	}
+
+	@Override
+
+	public String toString() {
+		return String.format("%s(%d)원", pName, price);
+	}
+
+}
