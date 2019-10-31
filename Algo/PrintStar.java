7673024d49b8c0506451ed692382a5a49e495a5d
Han Ji Hye <37392113+jh1023@users.noreply.github.com>
2019-10-31 15:50:08 +0900

    Update PrintStar.java

--- Algo/PrintStar.java
+++ Algo/PrintStar.java
@@ -34,5 +34,20 @@ public class PrintStar {
 		}
 
 	}
+	
+/*		public static void reversPrintStar(int line) {
+		for (int z = 0; z < line; z++) {
+			for (int i = 0; i < line; i++) {
+				if (i < (line - (z + 1))) {
+					System.out.print("0");
+				} else {
+					System.out.print("*");
+				}
+			}
+
+			System.out.println();
+		}
+
+	}*/
 
 }
